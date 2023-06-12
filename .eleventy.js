8306db745e132141897ca90a0bf1684eba608485
Nate Donato <natedonato@gmail.com>
2023-06-12 11:14:31 -0700

    add 11ty-img conversion, sort by git date in prod

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,35 @@ const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 const inspect = require("util").inspect;
+const eleventyWebcPlugin = require("@11ty/eleventy-plugin-webc");
+const { eleventyImagePlugin } = require("@11ty/eleventy-img");
 
 
 module.exports = function (eleventyConfig) {
+  // WebC
+  eleventyConfig.addPlugin(eleventyWebcPlugin, {
+    components: [
+      // …
+      // Add as a global WebC component
+      "npm:@11ty/eleventy-img/*.webc",
+    ],
+  });
+
+  // Image plugin
+  eleventyConfig.addPlugin(eleventyImagePlugin, {
+    // Set global default options
+    formats: ["webp", "jpeg"],
+    urlPath: "/img/",
+
+    // Notably `outputDir` is resolved automatically
+    // to the project output directory
+
+    defaultAttributes: {
+      loading: "lazy",
+      // decoding: "async",
+    },
+  });
+
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
 
@@ -28,9 +54,9 @@ module.exports = function (eleventyConfig) {
 
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
-    "./src/admin/config.yml": "./admin/config.yml"
+    "./src/admin/config.yml": "./admin/config.yml",
   });
-  
+
   eleventyConfig.addPassthroughCopy("./src/static/css/global.css");
 
   // Copy Image Folder to /_site
@@ -58,7 +84,6 @@ module.exports = function (eleventyConfig) {
     "debug",
     (content) => `<pre>${inspect(content)}</pre>`
   );
-
 
   // Let Eleventy transform HTML files as nunjucks
   // So that we can use .html instead of .njk
--- src/tonioposts/tonioposts.11tydata.js
+++ src/tonioposts/tonioposts.11tydata.js
@@ -7,8 +7,8 @@ let data = {
 };
 data.test123 = "js activated"
 
-// if(process.env.NODE_ENV === "production") {
-	data.date = "git Last Modified";
-// }
+if(process.env.NODE_ENV === "production") {
+	data.date = "git Created";
+}
 
 module.exports = data;
