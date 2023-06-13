53b1b0a4a1f3d9693deb3f9cfbc740a23c6f99de
Nate Donato <natedonato@gmail.com>
2023-06-13 12:16:18 -0700

    add window drag function

--- .eleventy.js
+++ .eleventy.js
@@ -3,34 +3,94 @@ const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 const inspect = require("util").inspect;
-const eleventyWebcPlugin = require("@11ty/eleventy-plugin-webc");
-const { eleventyImagePlugin } = require("@11ty/eleventy-img");
+// const eleventyWebcPlugin = require("@11ty/eleventy-plugin-webc");
+// const { eleventyImagePlugin } = require("@11ty/eleventy-img");
+const Image = require("@11ty/eleventy-img");
+
 
 
 module.exports = function (eleventyConfig) {
-  // WebC
-  eleventyConfig.addPlugin(eleventyWebcPlugin, {
-    components: [
-      // …
-      // Add as a global WebC component
-      "npm:@11ty/eleventy-img/*.webc",
-    ],
-  });
+  // 	eleventyConfig.addShortcode("image", async function(src, alt, sizes = "100vw") {
+  // 	// if(alt === undefined) {
+  // 	// 	// You bet we throw an error on missing alt (alt="" works okay)
+  // 	// 	throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+  // 	// }
+  // 	let metadata = await Image(src, {
+  // 		widths: [300, 600],
+  // 		formats: ['webp', 'jpeg']
+  // 	});
 
-  // Image plugin
-  eleventyConfig.addPlugin(eleventyImagePlugin, {
-    // Set global default options
-    formats: ["webp", "jpeg"],
-    urlPath: "/img/",
+  // 	let lowsrc = metadata.jpeg[0];
+  // 	let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
 
-    // Notably `outputDir` is resolved automatically
-    // to the project output directory
+  // 	return `<picture>
+  // 		${Object.values(metadata).map(imageFormat => {
+  // 			return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
+  // 		}).join("\n")}
+  // 			<img
+  // 				src="${lowsrc.url}"
+  // 				width="${highsrc.width}"
+  // 				height="${highsrc.height}"
+  // 				alt="${alt}"
+  // 				loading="lazy"
+  // 				decoding="async">
+  // 		</picture>`;
+  // });
 
-    defaultAttributes: {
-      loading: "lazy",
-      // decoding: "async",
-    },
-  });
+  // // --- START, eleventy-img
+  // function imageShortcode(src, alt, sizes = "(min-width: 1024px) 100vw, 50vw") {
+  //   console.log(`Generating image(s) from:  ${src}`);
+  //   let options = {
+  //     widths: [600, 900, 1500],
+  //     formats: ["webp", "jpeg"],
+  //     urlPath: "./static/img/",
+  //     outputDir: "./_site/static/img/",
+  //     filenameFormat: function (id, src, width, format, options) {
+  //       const extension = path.extname(src);
+  //       const name = path.basename(src, extension);
+  //       return `${name}-${width}w.${format}`;
+  //     },
+  //   };
+
+  //   // generate images
+  //   Image(src, options);
+
+  //   let imageAttributes = {
+  //     alt,
+  //     sizes,
+  //     loading: "lazy",
+  //     decoding: "async",
+  //   };
+  //   // get metadata
+  //   metadata = Image.statsSync(src, options);
+  //   return Image.generateHTML(metadata, imageAttributes);
+  // }
+  // eleventyConfig.addShortcode("image", imageShortcode);
+  // // --- END, eleventy-img
+
+  // // // WebC
+  // // eleventyConfig.addPlugin(eleventyWebcPlugin, {
+  // //   components: [
+  // //     // …
+  // //     // Add as a global WebC component
+  // //     "npm:@11ty/eleventy-img/*.webc",
+  // //   ],
+  // // });
+
+  // // // Image plugin
+  // // eleventyConfig.addPlugin(eleventyImagePlugin, {
+  // //   // Set global default options
+  // //   formats: ["webp", "jpeg"],
+  // //   urlPath: "/img/",
+
+  // //   // Notably `outputDir` is resolved automatically
+  // //   // to the project output directory
+
+  // //   defaultAttributes: {
+  // //     loading: "lazy",
+  // //     // decoding: "async",
+  // //   },
+  // // });
 
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
