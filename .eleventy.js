cdcb4d8e70a2dab0888dde3d52230a691412770a
Nate Donato <natedonato@gmail.com>
2023-06-13 17:50:15 -0700

    add functional webp image processing and workaround for missing file issue

--- .eleventy.js
+++ .eleventy.js
@@ -3,94 +3,37 @@ const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 const inspect = require("util").inspect;
-// const eleventyWebcPlugin = require("@11ty/eleventy-plugin-webc");
-// const { eleventyImagePlugin } = require("@11ty/eleventy-img");
 const Image = require("@11ty/eleventy-img");
 
-
-
 module.exports = function (eleventyConfig) {
-  // 	eleventyConfig.addShortcode("image", async function(src, alt, sizes = "100vw") {
-  // 	// if(alt === undefined) {
-  // 	// 	// You bet we throw an error on missing alt (alt="" works okay)
-  // 	// 	throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
-  // 	// }
-  // 	let metadata = await Image(src, {
-  // 		widths: [300, 600],
-  // 		formats: ['webp', 'jpeg']
-  // 	});
 
-  // 	let lowsrc = metadata.jpeg[0];
-  // 	let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+	eleventyConfig.addShortcode("image", async function (src, alt) {
+    if (alt === undefined) {
+      // You bet we throw an error on missing alt (alt="" works okay)
+      throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+    }
+    // console.log(src)
 
-  // 	return `<picture>
-  // 		${Object.values(metadata).map(imageFormat => {
-  // 			return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
-  // 		}).join("\n")}
-  // 			<img
-  // 				src="${lowsrc.url}"
-  // 				width="${highsrc.width}"
-  // 				height="${highsrc.height}"
-  // 				alt="${alt}"
-  // 				loading="lazy"
-  // 				decoding="async">
-  // 		</picture>`;
-  // });
-
-  // // --- START, eleventy-img
-  // function imageShortcode(src, alt, sizes = "(min-width: 1024px) 100vw, 50vw") {
-  //   console.log(`Generating image(s) from:  ${src}`);
-  //   let options = {
-  //     widths: [600, 900, 1500],
-  //     formats: ["webp", "jpeg"],
-  //     urlPath: "./static/img/",
-  //     outputDir: "./_site/static/img/",
-  //     filenameFormat: function (id, src, width, format, options) {
-  //       const extension = path.extname(src);
-  //       const name = path.basename(src, extension);
-  //       return `${name}-${width}w.${format}`;
-  //     },
-  //   };
-
-  //   // generate images
-  //   Image(src, options);
-
-  //   let imageAttributes = {
-  //     alt,
-  //     sizes,
-  //     loading: "lazy",
-  //     decoding: "async",
-  //   };
-  //   // get metadata
-  //   metadata = Image.statsSync(src, options);
-  //   return Image.generateHTML(metadata, imageAttributes);
-  // }
-  // eleventyConfig.addShortcode("image", imageShortcode);
-  // // --- END, eleventy-img
-
-  // // // WebC
-  // // eleventyConfig.addPlugin(eleventyWebcPlugin, {
-  // //   components: [
-  // //     // …
-  // //     // Add as a global WebC component
-  // //     "npm:@11ty/eleventy-img/*.webc",
-  // //   ],
-  // // });
-
-  // // // Image plugin
-  // // eleventyConfig.addPlugin(eleventyImagePlugin, {
-  // //   // Set global default options
-  // //   formats: ["webp", "jpeg"],
-  // //   urlPath: "/img/",
-
-  // //   // Notably `outputDir` is resolved automatically
-  // //   // to the project output directory
-
-  // //   defaultAttributes: {
-  // //     loading: "lazy",
-  // //     // decoding: "async",
-  // //   },
-  // // });
+    let metadata = await Image(src, {
+      widths: [600, 400],
+      formats: ["webp"],
+      urlPath: "/static/img/",
+      outputDir: "./_site/static/img/",
+      sharpOptions: {
+        animated: true,
+      },
+    });
+    // console.log(metadata)
+    let data = metadata.webp[metadata.webp.length - 1];
+    console.log(src, alt)
+    if(data.size === 0){
+      console.log(data.url)
+      console.log(data.size);
+      console.log(metadata);
+      data = metadata.webp.filter(el => el.size !== 0)[0] || data;
+    }
+    return `<img src="${data.url}" width="${data.width}" height="${data.height}" alt="${alt}" loading="lazy" decoding="async">`;
+  });
 
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
